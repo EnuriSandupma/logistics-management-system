0f18d1326eeda53ff4d8e2493fa14cb5c4c8cf35
EnuriSandupma <as20240510@sci.sjp.ac.lk>
2025-10-25 14:52:03 +0530

    add city function implemented successfully

--- logistics/main.c
+++ logistics/main.c
@@ -14,14 +14,19 @@
 // Global variables
 char cities[MAX_CITIES][MAX_NAME_LENGTH];
 int distances[MAX_CITIES][MAX_CITIES];
+int city_count = 0;
+
 
 
 
 
 // Function prototypes
 void displayMenu();
+void addCity();
 void loadFromFile();
+int findCity(char*name);
 void clearInputBuffer();
+
 
 // Main function with menu
 int main() {
@@ -38,6 +43,14 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
         clearInputBuffer();
+switch(choice) {
+            case 1:
+                addCity();
+                break;
+ exit(0);
+            default:
+                printf("\nInvalid choice! Please try again.\n");
+        }
 
 
     }
@@ -61,8 +74,41 @@ void displayMenu() {
     printf("10. Save and Exit\n");
     printf("========================================\n");
 }
+void addCity() {
+    if(city_count >= MAX_CITIES) {
+        printf("\nError: Maximum city limit reached!\n");
+        return;
+    }
+
+    char name[MAX_NAME_LENGTH];
+    printf("\nEnter city name: ");
+    fgets(name, MAX_NAME_LENGTH, stdin);
+    name[strcspn(name, "\n")] = 0; // Remove newline
 
 
+
+    strcpy(cities[city_count], name);
+
+
+    for(int i = 0; i <= city_count; i++) {
+        distances[city_count][i] = 0;
+        distances[i][city_count] = 0;
+    }
+city_count++;
+    printf("\nCity '%s' added successfully!\n", name);
+}
+
+
+
+    int findCity(char*name) {
+    for(int i=0;i<city_count;i++) {
+        if(strcasecmp(cities[i],name) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+    }
 void loadFromFile() {
     // Load cities and distances
     FILE *fp = fopen("routes.txt", "r");
